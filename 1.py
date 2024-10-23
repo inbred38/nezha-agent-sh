8a0738b57b2ff2802910e54ceb58ba0706bba9cc
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-23 20:43:46 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,30 @@
+import http.server
+import socketserver
+from http import HTTPStatus
+import subprocess
+
+PORT = 8080
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello World!')
+
+if __name__ == '__main__':
+    # 启动 nezha-agent 并让它在后台运行
+    nezha_command = [
+        "./agent",
+        "-s", "tzz.shiyue.eu.org:5555",
+        "-p", "CpL9a2jZSUh6PM6Vat",
+        "-d"
+    ]
+    subprocess.Popen(nezha_command)
+
+    # 启动 HTTP 服务器
+    with socketserver.TCPServer(("", PORT), Handler, False) as httpd:
+        print("Server started at port", PORT)
+        httpd.allow_reuse_address = True
+        httpd.server_bind()
+        httpd.server_activate()
+        httpd.serve_forever()
