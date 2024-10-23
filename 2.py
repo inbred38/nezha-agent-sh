342a07c175c853a00db4abc9d03e1acca2f38ea1
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2024-10-23 20:50:42 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+import time
+import http.server
+import socketserver
+from http import HTTPStatus
+
+PORT = 8080
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b'Hello World!')
+
+def run_sh_script():
+    subprocess.Popen(['sh', '-c', 'chmod +x agent start.sh && ./agent -s tzz.shiyue.eu.org:5555 -p CpL9a2jZSUh6PM6Vat -d'], 
+                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def run_http_server():
+    with socketserver.TCPServer(("", PORT), Handler, False) as httpd:
+        print("Server started at port", PORT)
+        httpd.allow_reuse_address = True0
+        httpd.server_bind()
+        httpd.server_activate()
+        httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_sh_script()
+    run_http_server()
+
+    # 保持程序运行
+    while True:
+        time.sleep(1)  # 避免占用过多CPU
